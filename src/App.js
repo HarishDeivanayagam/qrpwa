63e77058ae4b900ee67e6a2796795cf8696ce44f
Harish Deivanayagam <harish.deiva@gmail.com>
2022-12-22 15:10:46 +0530

    cams-list

--- src/App.js
+++ src/App.js
@@ -5,26 +5,45 @@ import { Html5Qrcode } from 'html5-qrcode';
 function App() {
 
   const [read, setRead] = useState("");
+  const [devices, setDevices] = useState([]);
+  const [cam, setCam] = useState("");
 
   useEffect(() => {
+
+    if (cam !== "") {
+      scan();
+    }
+
     init();
-  }, [])
+  }, [cam])
 
 
   const init = async () => {
     try {
-      let devices = await Html5Qrcode.getCameras();
-      let scanner = new Html5Qrcode("reader");
-      scanner.start(devices[0].id, { fps: 10, qrbox: { height: 250, width: 250 } }, (data) => setRead(data));
+      let cams = await Html5Qrcode.getCameras();
+      console.log(cams);
+      setDevices(cams);
     } catch (err) {
       console.log(err);
     }
   }
 
+  const scan = () => {
+    let scanner = new Html5Qrcode("reader");
+    scanner.start(cam, { fps: 10, qrbox: { height: 200, width: 200 } }, (data) => setRead(data));
+  }
+
+
   return (
-    <div>
+    <div style={{ display: "flex", justifyContent: "center", flexDirection: "column" }}>
       <h1>QR Reader</h1>
-      <div id='reader' style={{ height: "380px", width: "500px" }}></div>
+      <select value={cam} onChange={(e) => setCam(e.target.value)}>
+        <option value="">Select Camera</option>
+        {devices.map((device, index) => {
+          return <option key={index} value={device.id}>{device.label}</option>
+        })}
+      </select>
+      <div id='reader' style={{ height: "100%", width: "350px", marginTop: "1em" }}></div>
       <p>QR Value: {read ? read : "No Code Found"}</p>
     </div>
   );
