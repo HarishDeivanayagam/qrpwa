ad5e74e34fe5c575a613dc650292150cfb08a1fb
Harish Deivanayagam <harish.deiva@gmail.com>
2022-12-22 14:50:24 +0530

    html5qr

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Html5Qrcode } from 'html5-qrcode';
+
 
 function App() {
 
-  const videoRef = useRef();
+  const [read, setRead] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     init();
-  },[])
+  }, [])
+
 
   const init = async () => {
-    let media = await window.navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-    videoRef.current.srcObject = media;
+    try {
+      let devices = await Html5Qrcode.getCameras();
+      let scanner = new Html5Qrcode("reader");
+      scanner.start(devices[0].id, { fps: 10, qrbox: { height: 250, width: 250 } }, (data) => setRead(data));
+    } catch (err) {
+      console.log(err);
+    }
   }
-
 
   return (
     <div>
       <h1>QR Reader</h1>
-      <video ref={videoRef} autoPlay={true} height="100%" width="400px"></video>
-      <p>QR Value: {}</p>
+      <div id='reader' style={{ height: "380px", width: "500px" }}></div>
+      <p>QR Value: {read ? read : "No Code Found"}</p>
     </div>
   );
 }
